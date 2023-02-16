2cc07f2ae19b798bcd6e2df44bc8bf32c1961bd2
sydneyzhang18 <114766656+sydneyzhang18@users.noreply.github.com>
2023-02-16 11:58:32 -0800

    added right margin

--- lib/pages/resources.dart
+++ lib/pages/resources.dart
@@ -7,148 +7,144 @@ const buttonColor = const Color(0xFFDF742C);
 
 class Resources extends StatelessWidget {
   const Resources({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.white,
         appBar: const Header(),
         body: SingleChildScrollView(
-          child: Row(
+            child: Row(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisAlignment: MainAxisAlignment.start,
                 mainAxisSize: MainAxisSize.max,
                 children: <Widget>[
-                  SizedBox(
-                    width: 16,
-                  ),
-                  Flexible(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            SizedBox(
-                              width: 85.5,
-                            ),
-                            const Text(
-                              'Resources',
-                              textAlign: TextAlign.center,
-                              style: const TextStyle(
-                                fontSize: 34,
-                                fontFamily: 'Inter',
-                                fontWeight: FontWeight.w700,
-                                color: Colors.black,
-                              ),
-                            ),
-                          ],
-                        ),
-                        
-                        //search bar code
-                        IconButton(
-                          onPressed: () {
-                            Navigator.pushNamed(context, '/category_name');
-                          },
-                          icon: const Icon(Icons.search),
-                        ),
-
-                        //subtitle: categories
-                        const Text(
-                          'Categories',
-                          style: const TextStyle(
-                            fontSize: 22,
-                            fontFamily: 'Inter',
-                            fontWeight: FontWeight.w500,
-                            color: Color(0xFF111827),
+              SizedBox(
+                width: 16,
+              ),
+              Flexible(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          SizedBox(
+                            width: 85.5,
                           ),
-                        ),
-
-                        SizedBox(
-                          height: 16,
-                        ),
-
-                        //row 1 of two containers
-                        Row(
-                          children: [
-                            CategorySquare(
-                              catText: 'Medical',
-                              icon: Icons.medical_services_outlined,
+                          const Text(
+                            'Resources',
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                              fontSize: 34,
+                              fontFamily: 'Inter',
+                              fontWeight: FontWeight.w700,
+                              color: Colors.black,
                             ),
-                            SizedBox(
-                              width: 16,
-                            ),
-                            CategorySquare(
-                              catText: 'Legal',
-                              icon: Icons.balance_outlined,
-                            ),
-                          ],
-                        ),
+                          ),
+                        ],
+                      ),
 
-                        SizedBox(
-                          height: 16,
-                        ),
+                      //search bar code
+                      IconButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/category_name');
+                        },
+                        icon: const Icon(Icons.search),
+                      ),
 
-                        
-                        Row(
-                          children: [
-                            CategorySquare(
-                                catText: 'Housing',
-                                icon: Icons.house_outlined,
-                            ),
-                            SizedBox(
-                              width: 16,
-                            ),
-                            CategorySquare(
-                                catText: 'Domestic Violence',
-                                icon: Icons.shield_outlined,
-                            ),
-                          ],
+                      //subtitle: categories
+                      const Text(
+                        'Categories',
+                        style: const TextStyle(
+                          fontSize: 22,
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.w500,
+                          color: Color(0xFF111827),
                         ),
+                      ),
 
-                        SizedBox(
-                          height: 16,
-                        ),
+                      SizedBox(
+                        height: 16,
+                      ),
 
-                        InkWell(
-                          child: Container(
-                              height: 85.5,
-                              width: 358,
-                              decoration: BoxDecoration(
-                                color: themeColor,
-                                borderRadius:
-                                    BorderRadius.all(const Radius.circular(16)),
+                      //row 1 of two containers
+                      Row(
+                        children: [
+                          CategorySquare(
+                            catText: 'Medical',
+                            icon: Icons.medical_services_outlined,
+                          ),
+                          SizedBox(
+                            width: 16,
+                          ),
+                          CategorySquare(
+                            catText: 'Legal',
+                            icon: Icons.balance_outlined,
+                          ),
+                        ],
+                      ),
+
+                      SizedBox(
+                        height: 16,
+                      ),
+
+                      Row(
+                        children: [
+                          CategorySquare(
+                            catText: 'Housing',
+                            icon: Icons.house_outlined,
+                          ),
+                          SizedBox(
+                            width: 16,
+                          ),
+                          CategorySquare(
+                            catText: 'Domestic Violence',
+                            icon: Icons.shield_outlined,
+                          ),
+                        ],
+                      ),
+
+                      SizedBox(
+                        height: 16,
+                      ),
+
+                      InkWell(
+                        child: Container(
+                            height: 85.5,
+                            width: 358,
+                            decoration: BoxDecoration(
+                              color: themeColor,
+                              borderRadius:
+                                  BorderRadius.all(const Radius.circular(16)),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Text(
+                                'Resources',
+                                style: const TextStyle(
+                                    fontSize: 20,
+                                    fontFamily: 'Inter',
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.black),
                               ),
-                              child: Padding(
-                                padding: const EdgeInsets.all(16.0),
-                                child: Text(
-                                  'Resources',
-                                  style: const TextStyle(
-                                      fontSize: 20,
-                                      fontFamily: 'Inter',
-                                      fontWeight: FontWeight.w500,
-                                      color: Colors.black),
-                                ),
-                              )),
-                          onTap: () {
-                            Navigator.pushNamed(context, '/category_name');
-                          },
-                        ),
+                            )),
+                        onTap: () {
+                          Navigator.pushNamed(context, '/category_name');
+                        },
+                      ),
 
-                        SizedBox(
-                          height: 16,
-                        )
-                      ]),
-                  ),
-                  SizedBox(
-                    height: 16,
-                  )
-                ]
-            )
-        )
-    );
+                      SizedBox(
+                        height: 16,
+                      )
+                    ]),
+              ),
+              SizedBox(
+                width: 16,
+              )
+            ])));
   }
 }
