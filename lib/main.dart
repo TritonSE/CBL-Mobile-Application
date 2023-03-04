8020cfb31f18fec3409e89bf2f9774492355ec05
2s2e <stevendiwenshi1113@gmail.com>
2023-03-04 12:14:33 -0800

    Initial attempts at routing: imported authtest code to lib/main.dart, adding multiproviders, linking firebase

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,17 @@ import 'package:call_black_line/pages/take_action.dart';
 import 'package:call_black_line/widgets/custom_title.dart';
 import 'package:call_black_line/widgets/header.dart';
 import 'package:call_black_line/widgets/rounded_button_image.dart';
+
+import 'firebase_auth.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 // import 'package:call_black_line/take_action.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
@@ -20,23 +28,71 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Call Blackline',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(
-            seedColor: Colors.white,
+    return MultiProvider(
+        providers: [
+          Provider<AuthenticationService>(
+            create: (_) => AuthenticationService(FirebaseAuth.instance),
           ),
-        ),
-        routes: {
-          // '/': (context) => const SeekHelp(),
-          '/': (context) => const Donation(),
-          '/callTextNow': (context) => const CallTextNow(),
-          '/createAccount': (context) => const CreateAccount(),
-          '/takeAction': (context) => const TakeActionPage(),
-        }
-        // home: SeekHelp(),
+          StreamProvider(
+            create: (context) =>
+                context.read<AuthenticationService>().authStateChanges,
+            initialData: null,
+          ),
+        ],
+        child: MaterialApp(
+          title: 'Call Blackline',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(
+              seedColor: Colors.white,
+            ),
+          ),
+          routes: {
+            // '/': (context) => const SeekHelp(),
 
-        );
+            '/callTextNow': (context) => const CallTextNow(),
+            '/createAccount': (context) => const CreateAccount(),
+            '/takeAction': (context) => const TakeActionPage(),
+          },
+          home: const MyHomePage(title: 'Call Blackline'),
+        ));
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+
+    final firebaseuser = context.watch<User?>();
+
+    if (firebaseuser == null) {
+      return const CreateAccount();
+    } else {
+      return const SeekHelp();
+    }
   }
 }
 
