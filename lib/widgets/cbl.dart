638ac01549010d92587c696a0b89f973a581dd3f
Aaron Chan <42254254+aaronchan32@users.noreply.github.com>
2023-03-16 00:05:11 -0700

    added common variable class named CBL

---
+++ lib/widgets/cbl.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CBL {
+  static int primaryOrange = 0xffDF742C;
+  static int primaryVariantOrange = 0xffD55B26;
+  static int black = 0xff000000;
+  static int lightGray = 0xffB4B4B4;
+  static int success = 0xff3BB966;
+  static int blue = 0xff66A0F5;
+  static int textGray = 0xff909090;
+
+  static String fontFamily = "Inter";
+  static FontWeight bold = FontWeight.w500;
+  static double padding = 16;
+}
