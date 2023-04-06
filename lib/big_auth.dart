78c04124b7660cee6b1e592f612036c37d801bf2
2s2e <stevendiwenshi1113@gmail.com>
2023-04-06 14:43:46 -0700

    Tweaked big_auth.dart, signUp now takes in context to call authentication methods

--- lib/big_auth.dart
+++ lib/big_auth.dart
@@ -1,9 +1,10 @@
 // This file contains the main functions for signing up and deleting the account.
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'user.dart';
 import 'auth_methods.dart';
-
 
 class UserDataWPassword {
   final String username;
@@ -12,31 +13,49 @@ class UserDataWPassword {
   final String password;
 
   UserDataWPassword(
-      {required this.username, required this.phoneNumber, required this.email, required this.password});
+      {required this.username,
+      required this.phoneNumber,
+      required this.email,
+      required this.password});
 
   Map<String, dynamic> toMap() {
-    return {'username': username, 'phoneNumber': phoneNumber, 'email': email,'password': password};
+    return {
+      'username': username,
+      'phoneNumber': phoneNumber,
+      'email': email,
+      'password': password
+    };
   }
 }
-class UserRepository {
 
-  Future<Object> signUp(UserDataWPassword user) async {
+class SignUpUtils {
+  final UserRepository userRepository = UserRepository();
+  Future<Object> signUp(BuildContext context, UserDataWPassword user) async {
     try {
-      
+      int result = context.read<AuthenticationService>().signIn(
+          email: "stevendiwenshi1113@gmail.com", password: "123456") as int;
+
+      if (result == 400) {
+        UserData userData = UserData(
+            username: user.username,
+            phoneNumber: user.phoneNumber,
+            email: user.email);
+        userRepository.addUser(userData);
+        return 400;
+      }
     } catch (e) {
       return {'status': 'ERROR', 'message': e.toString()};
     }
-    
+    throw {
+      {'status': 'ERROR', 'message': 'Something went wrong.'}
+    };
   }
 
   Future<void> deleteAccount(String id) async {
-    try {
-
-    } catch (e) {
+    try {} catch (e) {
       rethrow;
     }
   }
-
 }
 
 /**
