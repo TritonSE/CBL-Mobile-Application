b08d9286ed7c983ac7c3d1a8b9dd114225287e14
Aaron Chan <42254254+aaronchan32@users.noreply.github.com>
2023-04-16 23:57:00 -0700

    Feat: added profile page

---
+++ lib/pages/profile.dart
@@ -0,0 +1,93 @@
+import 'package:call_black_line/widgets/cbl.dart';
+import 'package:call_black_line/widgets/orange_button.dart';
+import 'package:flutter/material.dart';
+import '../widgets/custom_navbar.dart';
+import '../widgets/custom_title.dart';
+import '../widgets/header.dart';
+
+class Profile extends StatelessWidget {
+  const Profile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const username = 'John Doe';
+    const email = 'johndoe@example.com';
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      bottomNavigationBar: const CustomNavBar(
+        currentPage: 'Profile',
+      ), //Available: Resources, Seek Help, Profile
+      appBar: const Header(
+        isHome: true,
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: CBL.padding),
+        child: Column(
+          children: [
+            const CustomTitle(text: 'Profile'),
+            const SizedBox(height: 50),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 8.0),
+                  child: Icon(Icons.person, color: Color(CBL.primaryOrange)),
+                ),
+                Text(
+                  'Username: ',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontFamily: CBL.fontFamily,
+                    fontWeight: CBL.bold,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+                Text(
+                  username,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontFamily: CBL.fontFamily,
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(height: 25),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 8.0),
+                  child: Icon(Icons.email, color: Color(CBL.primaryOrange)),
+                ),
+                Text(
+                  'Email: ',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontFamily: CBL.fontFamily,
+                    fontWeight: CBL.bold,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+                Text(
+                  email,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontFamily: CBL.fontFamily,
+                  ),
+                )
+              ],
+            ),
+            const Spacer(),
+            const Padding(
+              padding: EdgeInsets.only(bottom: 50),
+              child: OrangeButton(
+                buttonText: 'Logout',
+                width: 146,
+                // onTap: () => setState(() => subscribed = true),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
