2153842a2e36866a3eeb45ce6d3bba03834f713b
shreyagupta112 <94256244+shreyagupta112@users.noreply.github.com>
2023-04-26 16:37:18 -0700

    Display email on profile page

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -12,30 +12,41 @@ import '../auth_methods.dart';
 class Profile extends StatelessWidget {
   const Profile({super.key});
 
-  Future<String> getUsername(User? firebaseuser) async {
-    String email = firebaseuser!.email!;
-    // Get a reference to the Firestore collection
-    CollectionReference users = FirebaseFirestore.instance.collection('users');
-
-    // Create a query to search for documents where the email field matches the given email
-    QuerySnapshot querySnapshot =
-        await users.where('email', isEqualTo: email).get();
-
-    // If there is a document that matches the query, return it
-    if (querySnapshot.docs.isNotEmpty) {
-      DocumentSnapshot? data = querySnapshot.docs.first;
-      return data!['email'];
+  Future<String> getUsername(BuildContext context) async {
+    /*
+    final User? user = FirebaseAuth.instance.currentUser;
+    final String? userId = user?.email;
+    final docRef = db.collection("users").doc("e@gmail.com");
+    docRef.get().then(
+      (DocumentSnapshot doc) {
+        final data = doc.data() as Map<String, dynamic>;
+        return data['username'];
+        
+      },
+      onError: (e) => print("Error getting document: $e"),
+    ); 
+    */
+    final User? user = FirebaseAuth.instance.currentUser;
+    final String? userId = user?.email;
+    final firebaseuser = context.watch<User?>();
+    String? email = firebaseuser!.email;
+    try {
+      DocumentReference<Map<String, dynamic>> docRef =
+          FirebaseFirestore.instance.collection('users').doc(email);
+      DocumentSnapshot<Map<String, dynamic>> docSnapshot = await docRef.get();
+      print(10);
+      return docSnapshot['username'];
+    } catch (e) {
+      print('Error getting user name: $e');
+      return " ";
     }
-
-    // Otherwise, return null
-    return "Not found";
   }
 
   @override
   Widget build(BuildContext context) {
     final firebaseuser = context.watch<User?>();
-    var username = "John Doe";
-    String? email = "Temp";
+    String? email = firebaseuser?.email;
+
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
@@ -55,29 +66,39 @@ class Profile extends StatelessWidget {
           children: [
             const CustomTitle(text: 'Profile'),
             const SizedBox(height: 50),
-            Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(right: 8.0),
-                  child: Icon(Icons.person, color: Color(CBL.primaryOrange)),
-                ),
-                Text(
-                  'Username: ',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontFamily: CBL.fontFamily,
-                    fontWeight: CBL.bold,
-                  ),
-                  textAlign: TextAlign.left,
-                ),
-                Text(
-                  username,
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontFamily: CBL.fontFamily,
-                  ),
-                )
-              ],
+            FutureBuilder<String>(
+              future: getUsername(context),
+              builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                if (snapshot.hasData) {
+                  return Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(right: 8.0),
+                        child:
+                            Icon(Icons.person, color: Color(CBL.primaryOrange)),
+                      ),
+                      Text(
+                        'Username: ',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: CBL.fontFamily,
+                          fontWeight: CBL.bold,
+                        ),
+                        textAlign: TextAlign.left,
+                      ),
+                      Text(
+                        snapshot.data!,
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: CBL.fontFamily,
+                        ),
+                      )
+                    ],
+                  );
+                } else {
+                  return CircularProgressIndicator();
+                }
+              },
             ),
             const SizedBox(height: 25),
             Row(
