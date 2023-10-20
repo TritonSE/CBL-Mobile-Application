8efed454256fbf191a3e1c1e5853ee50fd6cf4ec
2s2e <stevendiwenshi1113@gmail.com>
2023-10-20 16:59:28 -0700

    Attempted to integrate google sign in with front end, still working

--- lib/auth_methods.dart
+++ lib/auth_methods.dart
@@ -90,14 +90,15 @@ class AuthenticationService {
             await auth.signInWithCredential(credential);
 
         user = userCredential.user;
+        print(user);
       } on FirebaseAuthException catch (e) {
         if (e.code == 'account-exists-with-different-credential') {
-          // handle the error here
+          return null;
         } else if (e.code == 'invalid-credential') {
-          // handle the error here
+          return null;
         }
       } catch (e) {
-        // handle the error here
+        return null;
       }
     }
 
