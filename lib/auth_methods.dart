ff90bfd5d11af15d50caebd4de04b3de67abab99
2s2e <stevendiwenshi1113@gmail.com>
2023-10-19 20:24:46 -0700

    Got started with authentication for google

--- lib/auth_methods.dart
+++ lib/auth_methods.dart
@@ -1,4 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 // import '/user.dart' as users;
 
 /// Help used:
@@ -63,5 +65,42 @@ class AuthenticationService {
     } on FirebaseAuthException catch (e) {
       return e.message!;
     }
+  }
+
+  Future<User?> signInWithGoogle({required BuildContext context}) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user;
+
+    final GoogleSignIn googleSignIn = GoogleSignIn();
+
+    final GoogleSignInAccount? googleSignInAccount =
+        await googleSignIn.signIn();
+
+    if (googleSignInAccount != null) {
+      final GoogleSignInAuthentication googleSignInAuthentication =
+          await googleSignInAccount.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleSignInAuthentication.accessToken,
+        idToken: googleSignInAuthentication.idToken,
+      );
+
+      try {
+        final UserCredential userCredential =
+            await auth.signInWithCredential(credential);
+
+        user = userCredential.user;
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'account-exists-with-different-credential') {
+          // handle the error here
+        } else if (e.code == 'invalid-credential') {
+          // handle the error here
+        }
+      } catch (e) {
+        // handle the error here
+      }
+    }
+
+    return user;
   }
 }
--- lib/widgets/social_media_button.dart
+++ lib/widgets/social_media_button.dart
@@ -1,5 +1,7 @@
 import 'package:call_black_line/widgets/cbl.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class SocialMedia extends StatelessWidget {
   const SocialMedia({super.key});
@@ -7,6 +9,9 @@ class SocialMedia extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var mediaWidth = MediaQuery.of(context).size.width;
+    final FirebaseAuth _auth = FirebaseAuth.instance;
+    final GoogleSignIn googleSignIn = GoogleSignIn();
+
     return Flex(
       direction:
           mediaWidth < CBL.smallMediaWidth ? Axis.vertical : Axis.horizontal,
@@ -147,7 +152,42 @@ class SocialMedia extends StatelessWidget {
                 ],
               ),
             ),
-            onTap: () {},
+            onTap: () async {
+              try {
+                // Trigger the authentication flow
+                final GoogleSignInAccount? googleSignInAccount =
+                    await googleSignIn.signIn();
+
+                if (googleSignInAccount != null) {
+                  Navigator.pushNamed(context, '/callTextNow');
+                }
+
+                // Obtain the auth details from the request
+                if (googleSignInAccount != null) {
+                  final GoogleSignInAuthentication googleSignInAuthentication =
+                      await googleSignInAccount.authentication;
+
+                  final AuthCredential credential =
+                      GoogleAuthProvider.credential(
+                    accessToken: googleSignInAuthentication.accessToken,
+                    idToken: googleSignInAuthentication.idToken,
+                  );
+
+                  final UserCredential authResult =
+                      await _auth.signInWithCredential(credential);
+                  final User? user = authResult.user;
+
+                  if (user != null &&
+                      !user.isAnonymous &&
+                      await user.getIdToken() != null) {
+                    Navigator.pushNamed(context, '/callTextNow');
+                  }
+                }
+              } catch (error) {
+                print(error);
+                return null;
+              }
+            },
           ),
         ),
       ],
