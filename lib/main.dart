88520ecab0691073b1779e00667e874cb33911cd
Philip Zhang <code@philipzha.ng>
2023-11-03 18:07:33 -0700

    add ios config

--- lib/main.dart
+++ lib/main.dart
@@ -17,11 +17,14 @@ import 'auth_methods.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future main() async {
   //setting up firebase
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const MyApp());
 }
