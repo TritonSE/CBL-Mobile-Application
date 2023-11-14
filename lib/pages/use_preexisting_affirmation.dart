0bdf6288680515373f03b985c4c2026f934049bc
Aaron Chan <42254254+aaronchan32@users.noreply.github.com>
2023-11-14 15:46:01 -0800

    Feat: add loading spinner to existing affirmations

--- lib/pages/use_preexisting_affirmation.dart
+++ lib/pages/use_preexisting_affirmation.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 
+import 'package:call_black_line/widgets/cbl.dart';
 import 'package:call_black_line/widgets/orange_button.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -23,6 +24,7 @@ class UsePreexistingAffirmation extends StatefulWidget {
 class _UsePreexistingAffirmationState extends State<UsePreexistingAffirmation> {
   late List<AffirmationWithUsername> affirmations = [];
   int selectedIndex = -1;
+  bool loading = true;
 
   @override
   void initState() {
@@ -34,7 +36,9 @@ class _UsePreexistingAffirmationState extends State<UsePreexistingAffirmation> {
     final affirmationRepository = AffirmationRepository();
     affirmations = await affirmationRepository.getAffirmationList();
     log('Loaded list of affirmations of length: ${affirmations.length}');
-    setState(() {});
+    setState(() {
+      loading = false;
+    });
   }
 
   @override
@@ -43,169 +47,163 @@ class _UsePreexistingAffirmationState extends State<UsePreexistingAffirmation> {
       resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
       appBar: Header(onBackButtonPressed: () {
+        Navigator.pushNamed(context, '/resources');
         //placeholder
       }),
       bottomNavigationBar: const CustomNavBar(
         currentPage: 'Resources',
       ),
       body: SingleChildScrollView(
-          child: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 30,
-        ),
-        child: Column(
-          children: [
-            const CustomTitle(text: 'Affirmation'),
-            const SizedBox(
-              width: 400,
-              child: Text(
-                'Lorem ipsum dolor sit amet consectetur. Magna mattis lorem auctor tortor ut nunc lacus at in. Tellus tempor a habitasse ac augue eu.',
-                style: TextStyle(
-                    fontSize: 17,
-                    fontFamily: 'Inter',
-                    fontWeight: FontWeight.w500),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            Padding(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 30,
+          ),
+          child: Column(
+            children: [
+              const CustomTitle(text: 'Affirmation'),
+              if (loading)
+                CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(
+                      Color(CBL.primaryVariantOrange)),
+                ),
+              Padding(
                 padding: const EdgeInsets.only(top: 24, bottom: 24),
                 child: SizedBox(
-                    height: 380,
-                    width: 400,
-                    child: ListView.separated(
-                        itemCount: affirmations.length,
-                        separatorBuilder: (context, index) {
-                          return const SizedBox(height: 16);
-                        },
-                        itemBuilder: (context, index) {
-                          return GestureDetector(
-                              onTap: () {
-                                setState(() {
-                                  selectedIndex = index;
-                                });
-                              },
-                              child: Container(
-                                  width: double.infinity,
-                                  height: 150,
-                                  decoration: BoxDecoration(
-                                      color: selectedIndex == index
-                                          ? const Color(0xFFDF742C)
-                                          : const Color(0xFFFFC596),
-                                      borderRadius: BorderRadius.circular(8)),
-                                  child: Padding(
-                                      padding: const EdgeInsets.symmetric(
-                                          horizontal: 15.0, vertical: 10.0),
-                                      child: Column(children: [
-                                        DropCapText(affirmations[index].text,
-                                            style: TextStyle(
-                                                color: selectedIndex == index
-                                                    ? Colors.white
-                                                    : Colors.black,
-                                                fontSize: 21,
-                                                height: 1.1),
-                                            dropCapPosition:
-                                                DropCapPosition.end,
-                                            dropCap: DropCap(
-                                                width: 25,
-                                                height: 25,
-                                                child: IconButton(
-                                                  padding:
-                                                      const EdgeInsets.all(0),
-                                                  icon: Image.asset(
-                                                      'assets/images/edit.png'),
-                                                  onPressed: () {
-                                                    showDialog(
-                                                      context: context,
-                                                      builder: (context) {
-                                                        TextEditingController
-                                                            controller =
-                                                            TextEditingController(
-                                                                text: affirmations[
-                                                                        index]
-                                                                    .text);
-                                                        return AlertDialog(
-                                                          title: const Text(
-                                                              "Edit Affirmation"),
-                                                          content: TextField(
-                                                            controller:
-                                                                controller,
-                                                            decoration:
-                                                                const InputDecoration(
-                                                              hintText:
-                                                                  "Enter new text here",
-                                                            ),
-                                                          ),
-                                                          actions: <Widget>[
-                                                            TextButton(
-                                                              child: const Text(
-                                                                  "Cancel"),
-                                                              onPressed: () {
-                                                                Navigator.of(
-                                                                        context)
-                                                                    .pop();
-                                                              },
-                                                            ),
-                                                            TextButton(
-                                                              child: const Text(
-                                                                  "Save"),
-                                                              onPressed: () {
-                                                                setState(() {
-                                                                  affirmations[
-                                                                              index]
-                                                                          .text =
-                                                                      controller
-                                                                          .text;
-                                                                });
-                                                                Navigator.of(
-                                                                        context)
-                                                                    .pop();
-                                                              },
-                                                            ),
-                                                          ],
-                                                        );
+                  height: 380,
+                  width: 400,
+                  child: ListView.separated(
+                      itemCount: affirmations.length,
+                      separatorBuilder: (context, index) {
+                        return const SizedBox(height: 16);
+                      },
+                      itemBuilder: (context, index) {
+                        return GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              selectedIndex = index;
+                            });
+                          },
+                          child: Container(
+                            width: double.infinity,
+                            height: 150,
+                            decoration: BoxDecoration(
+                                color: selectedIndex == index
+                                    ? const Color(0xFFDF742C)
+                                    : const Color(0xFFFFC596),
+                                borderRadius: BorderRadius.circular(8)),
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 15.0, vertical: 10.0),
+                              child: Column(children: [
+                                DropCapText(affirmations[index].text,
+                                    style: TextStyle(
+                                        color: selectedIndex == index
+                                            ? Colors.white
+                                            : Colors.black,
+                                        fontSize: 21,
+                                        height: 1.1),
+                                    dropCapPosition: DropCapPosition.end,
+                                    dropCap: DropCap(
+                                        width: 25,
+                                        height: 25,
+                                        child: IconButton(
+                                          padding: const EdgeInsets.all(0),
+                                          icon: Image.asset(
+                                              'assets/images/edit.png'),
+                                          onPressed: () {
+                                            showDialog(
+                                              context: context,
+                                              builder: (context) {
+                                                TextEditingController
+                                                    controller =
+                                                    TextEditingController(
+                                                        text:
+                                                            affirmations[index]
+                                                                .text);
+                                                return AlertDialog(
+                                                  title: const Text(
+                                                      "Edit Affirmation"),
+                                                  content: TextField(
+                                                    controller: controller,
+                                                    decoration:
+                                                        const InputDecoration(
+                                                      hintText:
+                                                          "Enter new text here",
+                                                    ),
+                                                  ),
+                                                  actions: <Widget>[
+                                                    TextButton(
+                                                      child:
+                                                          const Text("Cancel"),
+                                                      onPressed: () {
+                                                        Navigator.of(context)
+                                                            .pop();
                                                       },
-                                                    );
-                                                  },
-                                                ))),
-                                        const SizedBox(
-                                          height: 12,
-                                        ),
-                                        Align(
-                                            alignment: Alignment.centerLeft,
-                                            child: Text(
-                                              'By: ${affirmations[index].username}',
-                                              style: TextStyle(
-                                                fontSize: 18,
-                                                color: selectedIndex == index
-                                                    ? Colors.white
-                                                    : Colors.black,
-                                              ),
-                                              textAlign: TextAlign.left,
-                                            ))
-                                      ]))));
-                        }))),
-            Align(
-                alignment: Alignment.centerRight,
-                child: OrangeButton(
-                  buttonText: 'Submit',
-                  width: 110,
-                  onTap: () {
-                    AffirmationRepository affirmationRepository =
-                        AffirmationRepository();
-                    Affirmation affirmation = Affirmation(
-                        text: affirmations[selectedIndex].text,
-                        uid: auth.currentUser?.uid ?? '',
-                        created: DateTime.now().millisecondsSinceEpoch);
-                    affirmationRepository.addAffirmation(affirmation);
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const AffirmationSent()));
-                  },
-                ))
-          ],
+                                                    ),
+                                                    TextButton(
+                                                      child: const Text("Save"),
+                                                      onPressed: () {
+                                                        setState(() {
+                                                          affirmations[index]
+                                                                  .text =
+                                                              controller.text;
+                                                        });
+                                                        Navigator.of(context)
+                                                            .pop();
+                                                      },
+                                                    ),
+                                                  ],
+                                                );
+                                              },
+                                            );
+                                          },
+                                        ))),
+                                const SizedBox(
+                                  height: 12,
+                                ),
+                                Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: Text(
+                                    'By: ${affirmations[index].username}',
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      color: selectedIndex == index
+                                          ? Colors.white
+                                          : Colors.black,
+                                    ),
+                                    textAlign: TextAlign.left,
+                                  ),
+                                )
+                              ]),
+                            ),
+                          ),
+                        );
+                      }),
+                ),
+              ),
+              Align(
+                  alignment: Alignment.centerRight,
+                  child: OrangeButton(
+                    buttonText: 'Submit',
+                    width: 110,
+                    onTap: () {
+                      AffirmationRepository affirmationRepository =
+                          AffirmationRepository();
+                      Affirmation affirmation = Affirmation(
+                          text: affirmations[selectedIndex].text,
+                          uid: auth.currentUser?.uid ?? '',
+                          created: DateTime.now().millisecondsSinceEpoch);
+                      affirmationRepository.addAffirmation(affirmation);
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const AffirmationSent()));
+                    },
+                  ))
+            ],
+          ),
         ),
-      )),
+      ),
     );
   }
 }
